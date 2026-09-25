6cefb81766c2bccaa5ba6475bd48f78ae558fadb
Kamal singh <kamalsingh902752@gmail.com>
2026-09-25 06:47:35 +0000

    cors change

--- server/server.js
+++ server/server.js
@@ -23,7 +23,6 @@ app.use(cors({
   origin: [
     'http://localhost:5173', 
     'http://localhost:3000',
-    'https://job-portal-client-alpha-one.vercel.app' 
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
