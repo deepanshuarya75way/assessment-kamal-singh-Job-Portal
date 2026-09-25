90eae2423a46be9075fffcc9c0d4c240dfd7c4fd
Kamal singh <kamalsingh902752@gmail.com>
2026-09-25 08:17:12 +0000

    resume routes created

---
+++ server/routes/resume.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+import{
+ createResume,
+ getResumes,
+} from "../controllers/resumeController.js";
+
+const resumeRouter = express.Router();
+
+resumeRouter.post("/",createResume);
+resumeRouter.get("/", getResumes);
+
+export default resumeRouter;
